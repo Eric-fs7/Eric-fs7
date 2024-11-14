4a4929fa0024e2a257165201cf317a6708d474b7
vitor eduardo <ve141893@gmail.com>
2024-11-14 08:55:52 -0300

    integraçao banco

---
+++ conexao.php
@@ -0,0 +1,16 @@
+<?php
+$hostname = "localhoste";
+$bancodedados = "turistche.sql";
+$usuario = "";
+$senha = "";
+
+
+
+$mysqli = new mysqli ($hostname, $usuario, $senha, $bancodedados);
+if($mysqli -> connect_error) {
+    echo"falha ao conectar: (" . $mysqli-> connect_error . ")" . $mysqli-> connect_error
+
+
+
+
+}
